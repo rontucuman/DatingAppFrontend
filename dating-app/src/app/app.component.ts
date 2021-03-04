3dce98ee4fba7af30ac7b20b65b8135430fe9e3a
Ronald Tucuman Alarcon <ronald.tucuman@outlook.com>
2021-03-04 01:10:07 -0400

    added login form

--- dating-app/src/app/app.component.ts
+++ dating-app/src/app/app.component.ts
@@ -22,5 +22,6 @@ export class AppComponent implements OnInit {
     }, error => {
       console.log(error);
     });
+    console.log(this.users);
   }
 }
--- dating-app/src/app/app.module.ts
+++ dating-app/src/app/app.module.ts
@@ -1,21 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavComponent } from './nav/nav.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    NgbModule
+    NgbModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
