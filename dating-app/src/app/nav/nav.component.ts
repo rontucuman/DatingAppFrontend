9a4288a2810f89765ecb60ed1f801326bf310a17
Ronald Tucuman Alarcon <ronald.tucuman@outlook.com>
2021-03-04 23:04:42 -0400

    added log-in and log-out

--- dating-app/src/app/nav/nav.component.ts
+++ dating-app/src/app/nav/nav.component.ts
@@ -1,20 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
+
 export class NavComponent implements OnInit {
   model: any = {};
+  loggedIn: boolean = false;
 
-  constructor() { }
+  constructor(private accountService: AccountService) { }
 
   ngOnInit(): void {
   }
 
   login() {
-    console.log(this.model);
+    this.accountService.login(this.model).subscribe(response => {
+      console.log(response);
+      this.loggedIn = true;
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  logout() {
+    this.loggedIn = false;
   }
 
 }
